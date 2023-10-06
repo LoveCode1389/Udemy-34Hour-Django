5facef06d23e60cdc434a816abe980b4c1c2c974
LoveCode1389 <erfankhajezahde@gmail.com>
2023-10-06 19:29:42 +0330

    Udemy

--- food/views.py
+++ food/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Item
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    foods = Item.objects.all()
+
+    context = {
+        "foods": foods,
+    }
+    return render(request,'food/index.html', context)
 
 def item(request):
     return HttpResponse("<h1 style='text-align: center;'>This is an item view</h1>")
+
+def detail(request, id):
+    food = Item.objects.get(pk=id)
+    context = {
+        'food': food,
+    }
+
+    return render(request, 'food/detail.html', context)
