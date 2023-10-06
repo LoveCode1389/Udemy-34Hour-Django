4e8abf65a46a9af7e3ad3a16ed40341f3a9bf4a0
LoveCode1389 <erfankhajezahde@gmail.com>
2023-10-06 22:16:05 +0330

    Udemy

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('food/', include('food.urls')),
+    path('foods/', include('food.urls')),
     path("admin/", admin.site.urls),
 ]
