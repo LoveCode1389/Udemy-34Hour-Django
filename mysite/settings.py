37e299d9cb0eb086f44ee7095e5a348a567aeab4
LoveCode1389 <erfankhajezahde@gmail.com>
2023-10-12 22:09:05 +0330

    Udemy

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'food.apps.FoodConfig',
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'food.apps.FoodConfig',
 ]
 
 MIDDLEWARE = [
